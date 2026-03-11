154833edc0737341fc51a2ef3d15bb3c66efd54b
Adrien_D <noreply@linuxtricks.fr>
2026-03-11 22:39:20 +0100

    MàJ Fake IP

--- bash/fakeip.sh
+++ bash/fakeip.sh
@@ -4,7 +4,7 @@ echo "Génération IPv4 publique impossible :"
 for i in 1 2 3 4 5
 do
 	o1=$((RANDOM % 255))
-	o2=$((255 + RANDOM % 45)) # Octet "Hollywood" (255-299)
+	o2=$((256 + RANDOM % 44)) # Octet "Hollywood" (256-299)
 	o3=$((RANDOM % 255))
 	o4=$((RANDOM % 255))
 	ips=($o1 $o2 $o3 $o4)
@@ -19,9 +19,10 @@ echo "Generation IPv4 publique réservée à la doc :"
 for i in 1 2 3 4 5
 do
         # https://www.rfc-editor.org/rfc/rfc5737
-        case $((RANDOM % 2)) in
+        case $((RANDOM % 3)) in
             0) echo "198.51.100.$((RANDOM % 256))" ;; # 198.51.100.0/24
             1) echo "203.0.113.$((RANDOM % 256))" ;; # 203.0.113.0/24
+            2) echo "192.0.2.$((RANDOM % 256))" ;; # 192.0.2.0/24
         esac
 done
 
